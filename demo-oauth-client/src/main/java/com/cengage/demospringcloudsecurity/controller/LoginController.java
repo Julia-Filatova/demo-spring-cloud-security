1088fc1f72501ea2e4a6549b82887cff91ad2fae
yfilatova <yuliia.filatova@cengage.com>
2019-08-23 12:59:07 +0300

    Minor changes

--- demo-oauth-client/src/main/java/com/cengage/demospringcloudsecurity/controller/LoginController.java
+++ demo-oauth-client/src/main/java/com/cengage/demospringcloudsecurity/controller/LoginController.java
@@ -1,22 +1,13 @@
 package com.cengage.demospringcloudsecurity.controller;
 
-import java.security.Principal;
-import java.util.Collections;
-import java.util.Map;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/dashboard")
+@Controller
 public class LoginController {
 
-  @RequestMapping("/message")
-  public Map<String, Object> dashboard() {
-    return Collections.singletonMap("message", "User is logged in now!");
-  }
-
-  @RequestMapping("/user")
-  public Principal user(Principal user) {
-    return user;
+  @RequestMapping("/dashboard/login")
+  public String dashboard() {
+    return "redirect:/#/";
   }
 }
---
+++ demo-oauth-client/src/main/java/com/cengage/demospringcloudsecurity/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.cengage.demospringcloudsecurity.controller;
+
+import java.security.Principal;
+import java.util.Collections;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dashboard")
+public class UserController {
+
+  @RequestMapping("/message")
+  public Map<String, Object> dashboard() {
+    return Collections.singletonMap("message", "User is logged in now!");
+  }
+
+  @RequestMapping("/user")
+  public Principal user(Principal user) {
+    return user;
+  }
+}
